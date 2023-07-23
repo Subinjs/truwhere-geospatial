82667700ecce95d9c165a06759ec247c21634ecd
nijin26 <nijinazarkpz@gmail.com>
2023-07-23 22:54:01 +0530

    Add hover effect on training cards.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,58 @@
 (function () {
   "use strict";
 
+  const cards = document.querySelectorAll(".card");
+  let currentCard = null;
+
+  function rotateToMouse(e) {
+    if (!currentCard) return;
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const bounds = currentCard.getBoundingClientRect();
+    const leftX = mouseX - bounds.x;
+    const topY = mouseY - bounds.y;
+    const center = {
+      x: leftX - bounds.width / 2,
+      y: topY - bounds.height / 2,
+    };
+    const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+    currentCard.style.transform = `
+    scale3d(1.07, 1.07, 1.07)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance) * 2}deg
+    )
+  `;
+
+    currentCard.querySelector(".glow").style.backgroundImage = `
+    radial-gradient(
+      circle at
+      ${center.x * 2 + bounds.width / 2}px
+      ${center.y * 2 + bounds.height / 2}px,
+      #ffffff55,
+      #0000000f
+    )
+  `;
+  }
+
+  cards.forEach(($card) => {
+    $card.addEventListener("mouseenter", () => {
+      currentCard = $card;
+      document.addEventListener("mousemove", rotateToMouse);
+    });
+
+    $card.addEventListener("mouseleave", () => {
+      document.removeEventListener("mousemove", rotateToMouse);
+      currentCard.style.transform = "";
+      currentCard.style.background = "";
+      currentCard = null;
+    });
+  });
+
   /**
    * Easy selector helper function
    */
